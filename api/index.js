3f3850b8eb28a611fcdd7ba4739e161cf92c06e3
Jean Vera <jean@amicitech.com>
2020-11-25 11:41:15 -0500

    test nodeapp

--- api/index.js
+++ api/index.js
@@ -11,53 +11,6 @@ app.get('/api/test', (req, res) => {
     res.json({ 'message': 'Welcome test' })
 });
 
-app.get('/pi', (req, res) => {
-    res.json({ 'message': 'Welcome API' })
-});
-app.post('/api/posts', verifyToken, (req, res) => {
-    jwt.verify(req.token, 'secretKey', (err, authData) => {
-        if (err) {
-            res.sendStatus(403);
-        } else {
-            res.json({
-                message: 'Post created...',
-                authData
-            });
-        }
-    });
-});
-app.post('/api/login', (req, res) => {
-    const user = {
-        id: 1,
-        name: 'Jean'
-    }
-    jwt.sign({ user }, "secretKey", (err, token) => {
-        res.json({
-            user,
-            token
-        })
-    })
-
-});
-// Verify Token
-function verifyToken(req, res, next) {
-    // Get auth header value
-    const bearerHeader = req.headers['authorization'];
-    // Check if bearer is undefined
-    if (typeof bearerHeader !== 'undefined') {
-        // Split at the space
-        const bearer = bearerHeader.split(' ');
-        // Get token from array
-        const bearerToken = bearer[1];
-        // Set the token
-        req.token = bearerToken;
-        // Next middleware
-        next();
-    } else {
-        // Forbidden
-        res.status(403).json({ error: "Not Authorized" });
-    }
-}
 const port = process.env.PORT || 3000
 app.listen(port, (e) => {
     console.log("Listening on port " + port);
