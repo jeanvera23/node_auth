b2b4e9020311686f04f6ce33d5281f57ed3896e1
Jean Vera <jean@amicitech.com>
2020-11-25 12:02:10 -0500

    updated mongodb

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
 
 const app = express();
 
@@ -10,11 +12,22 @@ app.get('/jean', (req, res) => {
 app.get('/api/test', (req, res) => {
     res.json({ 'message': 'Welcome test' })
 });
-
+app.get('/mongodb', (req, res) => {
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("mydbtest");
+        dbo.collection("users").find({}, function (err, result) {
+            if (err) throw err;
+            res.json(result);
+            db.close();
+        });
+    });
+});
 const port = process.env.PORT || 3000
 app.listen(port, (e) => {
     console.log("Listening on port " + port);
 })
+
 module.exports = {
     app
 };
